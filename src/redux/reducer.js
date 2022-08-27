c0f2d45e0b206b10ac1f85d02d738f9d22d65023
nguyenduysu <su.nguyenduy.nb@gmail.com>
2022-08-28 00:38:54 +0700

    done todo app with redux core

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,19 @@
+import { combineReducers } from "redux"; 
+
 import filterReducer from "../components/Filters/FiltersSlice.js";
 import todoListReducer from "../components/TodoList/TodosSlice";
 
-const rootReducer = (state = {}, action) => {
-    console.log("state: ", state)
-    return {
-        filters: filterReducer(state.filters, action),
-        todoList: todoListReducer(state.todoList, action),
-    }
-}
+// const rootReducer = (state = {}, action) => {
+//     console.log("state: ", state)
+//     return {
+//         filters: filterReducer(state.filters, action),
+//         todoList: todoListReducer(state.todoList, action),
+//     }
+// }
+
+const rootReducer = combineReducers({
+    filters: filterReducer,
+    todoList: todoListReducer
+})
 
 export default rootReducer;
